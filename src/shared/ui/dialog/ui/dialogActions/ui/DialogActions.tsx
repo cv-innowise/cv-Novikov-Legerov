8fedcae02e7afccb0ac9cb8c4ed8f876ed3d6869
AlexLegerov <legerov13@gmail.com>
2025-09-30 16:07:44 +0300

    feat(users):added dialog for update user

--- src/shared/ui/dialog/ui/dialogActions/ui/DialogActions.tsx
+++ src/shared/ui/dialog/ui/dialogActions/ui/DialogActions.tsx
@@ -1,13 +1,15 @@
-import { DialogActions as MuiDialogActions, Button } from "@mui/material"
-import { hideDialog } from "@shared/ui/dialog/model/dialogService"
-import { DialogActionsProps } from "./DialogActions.props"
-import Loader from "@shared/ui/loader"
+import { Button, DialogActions as MuiDialogActions } from "@mui/material"
 import { useTranslations } from "next-intl"
+
+import { hideDialog } from "@shared/ui/dialog/model/dialogService"
+import Loader from "@shared/ui/loader"
+
+import { DialogActionsProps } from "./DialogActions.props"
 import { styles } from "./DialogActions.styles"
 
 const DialogActions = ({ loaders, confirmButtonText }: DialogActionsProps) => {
-    const isLoading = loaders.some(Boolean);
-    const t = useTranslations();
+	const isLoading = loaders?.some(Boolean)
+	const t = useTranslations()
 
 	return (
 		<MuiDialogActions sx={styles.container}>
